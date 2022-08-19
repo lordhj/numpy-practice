ee34c8be86ff49930fc8fdbb4315c27ac5a7cd31
Harshit Joshi <harshitjoshi450@gmail.com>
2022-08-20 00:25:37 +0530

    Basic commands

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-import numpy
+import numpy as np
+
+#Creating numpy array through a python list
+myList = [1, 2, 3, 4]
+x=np.array(myList)
+
+#Creating numpy array of a matrix
+myMatrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+y=np.array(myMatrix)
+
+#Using BuiltIN
+z = np.arange(0, 10, 2)
+zero_arr = np.zeros(6)
+zero_matrix = np.zeros((2,2))
+one_arr = np.ones(6) #Similar to zeros function
+linspaced_arr = np.linspace(0,10,4)
+iden_matrix = np.eye(2)
+
+#Random
+uniform_2_by_3 = np.random.rand(2,3)
+normal_2_by_3 = np.random.randn(2,3)
+random5_bw_1_to_100 = np.random.randint(0, 101, 5)
