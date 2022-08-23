bddbd7798b112c7d297d477b82b1a9e99b259cbf
Harshit Joshi <harshitjoshi450@gmail.com>
2022-08-23 13:23:11 +0530

    Creation codes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+"""
 #Creating numpy array through a python list
 myList = [1, 2, 3, 4]
 x=np.array(myList)
@@ -20,3 +21,10 @@ iden_matrix = np.eye(2)
 uniform_2_by_3 = np.random.rand(2,3)
 normal_2_by_3 = np.random.randn(2,3)
 random5_bw_1_to_100 = np.random.randint(0, 101, 5)
+np.random.seed(42)
+print(np.random.rand(4))
+
+arr=np.arange(0, 10)
+arr=arr.reshape(5, 2)
+print(arr)
+"""
